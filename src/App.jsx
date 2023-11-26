595b953d29f75ef03d4f8910f9082863b074b251
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2023-11-26 22:01:51 +0530

    From BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Login } from './pages/auth/Login'
 import { ExpenseVouge } from './pages/ExpenseVouge'
 import { useContext } from 'react'
