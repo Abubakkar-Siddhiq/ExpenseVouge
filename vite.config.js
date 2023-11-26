0a532b8772d693fd459e364bb354200882e18069
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2023-11-26 21:36:49 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,26 @@ import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({
+ command
+}) => {
+  if(command === 'build'){
+    return{
+      base: '/ExpenseVouge/',
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+    }
+  }
+  return{
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
